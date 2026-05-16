d59704eef597fe702fe4a3a9f2445fa508ef1591
iJuice <ilya.safa1337w@gmail.com>
2026-05-16 11:45:03 +0500

    feat: implemented WorkoutConfigRepositoryImpl.kt

---
+++ app/src/main/java/com/mobile/finsolve/app/movefasttdd/data/repository/WorkoutConfigRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.mobile.finsolve.app.movefasttdd.data.repository
+
+import com.mobile.finsolve.app.movefasttdd.data.local.WorkoutConfigDao
+import com.mobile.finsolve.app.movefasttdd.data.local.WorkoutConfigEntity
+import com.mobile.finsolve.app.movefasttdd.domain.model.WorkoutConfig
+import com.mobile.finsolve.app.movefasttdd.domain.repository.WorkoutConfigRepository
+import javax.inject.Inject
+
+class WorkoutConfigRepositoryImpl @Inject constructor(
+    private val dao: WorkoutConfigDao,
+) : WorkoutConfigRepository {
+
+    override suspend fun save(config: WorkoutConfig) {
+        dao.save(config.toEntity())
+    }
+
+    override suspend fun load(): WorkoutConfig? {
+        return dao.load()?.toDomain()
+    }
+}
+
+private fun WorkoutConfig.toEntity() = WorkoutConfigEntity(
+    reps = reps,
+    repDuration = repDuration,
+    restDuration = restDuration,
+)
+
+private fun WorkoutConfigEntity.toDomain() = WorkoutConfig(
+    reps = reps,
+    repDuration = repDuration,
+    restDuration = restDuration,
+)
