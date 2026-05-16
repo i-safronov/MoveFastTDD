942fff410d4f0a04a5562525b20be482effc3783
iJuice <ilya.safa1337w@gmail.com>
2026-05-16 18:12:26 +0500

    refactor: remove @Parcelize from WorkoutConfig

--- app/src/main/java/com/mobile/finsolve/app/movefasttdd/domain/model/WorkoutConfig.kt
+++ app/src/main/java/com/mobile/finsolve/app/movefasttdd/domain/model/WorkoutConfig.kt
@@ -1,11 +1,7 @@
 package com.mobile.finsolve.app.movefasttdd.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class WorkoutConfig(
     val reps: Int,
     val repDuration: Int,
     val restDuration: Int,
-) : Parcelable
+)
